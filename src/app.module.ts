7ce9f481336333a745ba2115d37e3a00ce007f6a
IliaPonomarev <seikadi123@gmail.com>
2023-12-21 23:31:48 +0300

    Added new service to docker-compose file

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,23 +5,25 @@ import { BullModule } from '@nestjs/bull';
 import { TasksModule } from './modules/tasks/tasks.module';
 import { AppLoggerMiddleware } from './middlewares/logger.middleware';
 import { NestMinioModule  } from 'nestjs-minio';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     BullModule.forRootAsync({
       useFactory: () => ({
         redis: {
-          host: 'redis',
-          port: 6379,
+          host: process.env.REDIS_HOST || 'redis',
+          port: parseInt(process.env.REDIS_PORT, 10) || 6379,
         },
       }),
     }),
     NestMinioModule.register({
-      endPoint: 'minio',
-      port: 9000,
-      useSSL: false,
-      accessKey: 'minioadmin',
-      secretKey: 'minioadmin',
+      endPoint: process.env.MINIO_ENDPOINT || 'minio',
+      port: parseInt(process.env.MINIO_PORT, 10) || 9000,
+      useSSL: process.env.MINIO_USE_SSL === 'true',
+      accessKey: process.env.MINIO_ACCESS_KEY || 'minioadmin',
+      secretKey: process.env.MINIO_SECRET_KEY || 'minioadmin',
     }),
     AuthModule, 
     FileProcessingModule,
